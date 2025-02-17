85d8a8d36e82821a4d1e3c93960178932bf07e8b
Sam Seid <samanthaseid11@gmail.com>
2025-02-17 17:00:06 -0500

    adding interaction to buttons

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float speed = 2;
+    //public float angle = 1;
+    //public float size = 1;
+
+    public SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,29 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        //pos.y += speed * Time.deltaTime;
+        transform.position = pos;
+/*
+        Vector3.one rot = transform.rotation;
+        rot.z += angle;
+        transform.rotation = rot;
+
+        Vector2 scale = transform.LocalScale;
+        scale.x += size * 2;
+        transform.LocalScale = scale;
+*/
     }
+    /*
+    public void Increase(float size)
+    {
+        spriteRenderer.transform.localScale = new Vector3(2f, 2f, 2f);
+    }
+
+    public void Decrease(float size)
+    {
+        spriteRenderer.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+    }
+    */
 }
--- Assets/Scripts/CreateBullet.cs
+++ Assets/Scripts/CreateBullet.cs
@@ -4,19 +4,53 @@ using UnityEngine;
 
 public class CreateBullet : MonoBehaviour
 {
+    //declare prefab gameobject
+    //public for inspector
+    public GameObject prefab;
+
+    public List<GameObject> createAmmo;
     // Start is called before the first frame update
     void Start()
     {
-        
+        createAmmo = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
+            GameObject bullets = Instantiate(prefab, transform);
+            createAmmo.Add(bullets);
+            //Instantiate(prefab, //position//, transform.rotation);
+            //makes prefab same rotation as spawner gameobject
+            //clone original thing, make the prefab
 
+            Destroy(bullets, 3);
+            //destroy prefab in 3 seconds}
         }
-        //if the left button mouse is clicked do this
+        /*
+        //change size
+        bullets.transform.localScale = Vector3.one * 2;
+
+        //change rotation
+        Bullet myScript = bullets.GetComponent<Bullet>();
+
+        if(myScript != null)
+        {
+            myScript.angle = Vector3.one * 25;
+        }
+        */
+    }
+    //if the left button mouse is clicked do this
+
+
+
+
+    public void create()
+    {
+        
+
+
     }
 }
